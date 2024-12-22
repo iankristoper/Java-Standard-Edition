9eaf01418abad893e371d2ec484f766ebac3df0c
iankristoper <iankristoper26@gmail.com>
2024-12-22 22:50:29 +0800

    minor changes

--- Java Multithreading/Multithreading/Countdown/src/main/java/com/ian/countdown/Countdown.java
+++ Java Multithreading/Multithreading/Countdown/src/main/java/com/ian/countdown/Countdown.java
@@ -1,6 +1,4 @@
 package com.ian.countdown;
- 
-
 
 
 /*
@@ -9,74 +7,74 @@ package com.ian.countdown;
 
 
 
+
 import java.util.Scanner;
+
+
+
 
 
 
 public class Countdown {
 
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Enter number of timers: ");
         int numberOfTimers = scanner.nextInt();
         scanner.nextLine();
+
+        int[] durations = new int[numberOfTimers];
+
         
-        Thread[] threads = new Thread[numberOfTimers];
-        
-        
-        //loop for creating threads 
-        for(int i = 0; i < numberOfTimers; i++) {    
-            
+        // Collect all durations first
+        for (int i = 0; i < numberOfTimers; i++) {
             System.out.print("Enter duration of timer " + (i + 1) + " in seconds: ");
-            int duration = scanner.nextInt();
-            scanner.nextLine();;sdd
-            
-            int timerNumber = i + 1; 
-            
-            
-            //create new thread for each timer 
+            durations[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        
+        // Create and start threads for all timers
+        Thread[] threads = new Thread[numberOfTimers];
+        for (int i = 0; i < numberOfTimers; i++) {
+            int timerNumber = i + 1;
+            int duration = durations[i];
             threads[i] = new Thread(() -> runTimer(timerNumber, duration));
-            threads[i].start();           
+            threads[i].start();
         }
+
         
-        
-        //wait for all timers to finish
-        for(Thread copy : threads) {          
+        // Wait for all timers to finish
+        for (Thread thread : threads) {
             try {
-                copy.join();
-            } catch(InterruptedException e) {
+                thread.join();
+            } catch (InterruptedException e) {
                 System.out.println("A thread was interrupted.");
-            }           
-        }
-        
-        
-        System.out.println("All timers have completed!");
-        scanner.close();
-        
-    }
-    
-    
-    //runtimer method
-    public static void runTimer(int timerNumber, int duration) {
-        
-        System.out.println("Timer " + timerNumber + " started with " + duration + " seconds.");
-        
-        for(int i = duration; i >= 0; i--) {
-            
-            System.out.println("Timer " + timerNumber + ": " + i + " seconds remaining");
-            
-            try {
-                Thread.sleep(1000);
-            } catch(InterruptedException e) {
-                System.out.println("Timer " + timerNumber + " was interrupted.");
-                Thread.currentThread().interrupt();
-                break;              
             }
         }
-        
+
+        System.out.println("All timers have completed!");
+        scanner.close();
+    }
+    
+
+    // Run timer method
+    public static void runTimer(int timerNumber, int duration) {
+        System.out.println("Timer " + timerNumber + " started with " + duration + " seconds.");
+
+        for (int i = duration; i >= 0; i--) {
+            System.out.println("Timer " + timerNumber + ": " + i + " seconds remaining");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Timer " + timerNumber + " was interrupted.");
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+
         System.out.println("Timer " + timerNumber + " has finished!");
-        
     }
 }
