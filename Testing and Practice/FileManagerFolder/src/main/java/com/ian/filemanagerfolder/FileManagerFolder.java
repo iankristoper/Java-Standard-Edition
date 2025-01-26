490a6f372200d5d82025f7abf6b992349c7b7f45
iankristoper <iankristoper26@gmail.com>
2025-01-27 00:05:55 +0800

    another changes and added new programs

---
+++ Testing and Practice/FileManagerFolder/src/main/java/com/ian/filemanagerfolder/FileManagerFolder.java
@@ -0,0 +1,128 @@
+package com.ian.filemanagerfolder;
+
+
+//This is a File manager folder where the user can create file, view file, delete file and others
+//
+
+
+import java.io. *;
+import java.util.*;
+
+
+
+public class FileManagerFolder {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
+
+
+
+
+
+
+//class for the data 
+//it incorporates encapsulation
+class FileData {
+    
+    private String name;
+    
+    public FileData(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+
+
+
+//class for the methods and functionality
+class FileManager {
+    
+    private static final String FOLDER_PATH = "./files/"; //directory for managing files
+    private File folder;
+    
+    public FileManager() {
+        folder = new File(FOLDER_PATH);
+        
+        if(!folder.exists()) {
+            folder.mkdirs();   //create the folder if it doesnt exists  
+        }
+    }
+    
+    
+    public void createFile(Scanner scanner) {
+        
+        System.out.print("Enter file name: ");
+        String fileName = scanner.nextLine();
+        
+        File newFile = new File(FOLDER_PATH + fileName);
+        
+        try {
+            if(newFile.createNewFile()) {
+                System.out.println("File created successfully: " + fileName);
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch(IOException e) {
+            System.out.println("An error occured: " + e.getMessage());
+        }
+        
+        
+    }  
+    
+    public void editFile(Scanner scanner) {
+        
+        System.out.print("Enter the name of the file to edit: ");
+        String fileName = scanner.nextLine();
+        
+        File file = new File(FOLDER_PATH + fileName);
+        
+        if(file.exists()) {
+            System.out.print("Enter the new content for the file name (type 'exit' to finish): ");
+            StringBuilder newContent = new StringBuilder();
+            String line;
+            
+            while(!(line = scanner.nextLine()).equalsIgnoreCase("exit")) {
+                newContent.append(line).append("\n");
+            }           
+            
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                writer.write(newContent.toString());
+                System.out.println("File edited successfully");
+            } catch(IOException e) {
+                System.out.println("An error occured: " + e.getMessage());
+            }
+            
+        }
+        
+    }
+    
+    public void removeFile() {
+        
+    }
+    
+    public void viewFile() {
+        
+    }
+    
+    public void viewAllFiles() {
+        
+    }
+    
+    public void checkProperties() {
+        
+    }
+    
+}
