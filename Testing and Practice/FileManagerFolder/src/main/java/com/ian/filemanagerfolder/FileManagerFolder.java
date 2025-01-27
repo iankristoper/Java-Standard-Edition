261715a66bcd6c2f010cacb8757b51aadc2a2cca
iankristoper <iankristoper26@gmail.com>
2025-01-28 04:15:42 +0800

    another changes

--- Testing and Practice/FileManagerFolder/src/main/java/com/ian/filemanagerfolder/FileManagerFolder.java
+++ Testing and Practice/FileManagerFolder/src/main/java/com/ian/filemanagerfolder/FileManagerFolder.java
@@ -14,13 +14,63 @@ public class FileManagerFolder {
 
     public static void main(String[] args) {
         
+        FileManager file = new FileManager();
         Scanner scanner = new Scanner(System.in);
+        boolean isProgramRunning = true;
+        
+        while(isProgramRunning) {
+            
+            System.out.println("Welcome to my own File Folder");
+            System.out.println("[1] Create File");
+            System.out.println("[2] Edit File");
+            System.out.println("[3] Remove File");
+            System.out.println("[4] View File");
+            System.out.println("[5] View All files");
+            System.out.println("[6] Check file Properties");
+            System.out.println("[7] Exit Program");
+            System.out.println("");
+            
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch(choice) {
+                case 1:
+                    file.createFile(scanner);
+                    break;
+                    
+                case 2:
+                    file.editFile(scanner);
+                    break;
+                    
+                case 3:
+                    file.removeFile(scanner);
+                    break; 
+                    
+                case 4:
+                    file.viewFile(scanner);
+                    break;
+                    
+                case 5:
+                    file.viewAllFiles();
+                    break;
+                    
+                case 6:
+                    file.checkProperties(scanner);
+                    break;
+                    
+                case 7:
+                    isProgramRunning = false;
+                    break;
+                    
+                default:
+                    System.out.println("Invalid choice, please try again");
+            }
+        }
+        
         
     }
 }
-
-
-
 
 
 
@@ -45,8 +95,6 @@ class FileData {
 
 
 
-
-
 //class for the methods and functionality
 class FileManager {
     
@@ -59,6 +107,7 @@ class FileManager {
         if(!folder.exists()) {
             folder.mkdirs();   //create the folder if it doesnt exists  
         }
+        
     }
     
     
@@ -78,8 +127,7 @@ class FileManager {
         } catch(IOException e) {
             System.out.println("An error occured: " + e.getMessage());
         }
-        
-        
+              
     }  
     
     public void editFile(Scanner scanner) {
@@ -153,14 +201,39 @@ class FileManager {
         } else {
             System.out.println("File not found");
         }
+        
     }
     
     public void viewAllFiles() {
         
+        File[] files = folder.listFiles();
+        
+        if(files != null && files.length > 0) {
+            System.out.println("Files in the folder: ");
+            
+            for(File copy : files) {
+                System.out.println(copy.getName());
+            }
+        } else {
+            System.out.println("No files found in the folder");
+        }
         
     }
     
-    public void checkProperties() {
+    public void checkProperties(Scanner scanner) {
+        
+        System.out.println("Enter the name of the file to check the properties: ");
+        String fileName = scanner.nextLine();
+        
+        File file = new File(FOLDER_PATH + fileName);
+        
+        if(file.exists()) {
+            System.out.println("Properties of " + fileName + ": ");
+            System.out.println("Size: " + file.length() + " bytes");
+            System.out.println("Last Modified: " + new Date(file.lastModified()));
+        } else {
+            System.out.println("File not found");
+        }
         
     }
     
